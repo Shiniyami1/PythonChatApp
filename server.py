020a5daf50eef605e065c96b7ae198d65c6fa94a
msharif9 <46731860+msharif9@users.noreply.github.com>
2019-11-30 10:57:11 -0500

    working multi room chat!

--- server.py
+++ server.py
@@ -3,54 +3,64 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-clients = {}
-addresses = {}
-HOST = ''
-PORT = 4200
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
 
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Welcome to SE3313 Project - Chat Application"
-                                  "\r\n\tPlease enter your username", "utf8"))
+        client.send(bytes("Please enter your username", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
+
 def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
+
     name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {disconnect} to exit. \r\n\tPlease enter the chatroom you wish to join' % name
+    welcome = 'Type {disconnect} to exit.'
     client.send(bytes(welcome, "utf8"))
-    clients[client] = name
-    first = True
+    
+    handle = {'socket' : client, 'room' : ''}
+    clients[name] = handle
+    roomPrompt = 'Please enter the chatroom you wish to join'
+    client.send(bytes(roomPrompt, "utf8"))
+
     while True:
         msg = client.recv(BUFSIZ)
-        if first:
-            clients['room'] = msg
-            now = 'Now chatting in '+msg.decode("utf8")+' room'
-            client.send(bytes(now, "utf8"))
+        if clients[name]['room'] == '':
+            room = msg.decode("utf8")
+            clients[name]['room'] = room
             msg = "%s has joined the chat!" % name
-            broadcast(bytes(msg, "utf8"), clients['room'])
-        if msg != bytes("{disconnect}", "utf8"):
-            broadcast(msg, name+": ",clients['room'])
+            broadcast(bytes(msg, "utf8"),room)
+        elif msg != bytes("{quit}", "utf8"):
+            broadcast(msg, room, name+": ")
         else:
-            client.send(bytes("{disconnect}", "utf8"))
+            client.send(bytes("{quit}", "utf8"))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"), clients['room'])
+            broadcast(bytes("%s has left the chat." % name, "utf8"), room)
             break
+
 
 def broadcast(msg, room, prefix=""):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
-    for sock in clients:
-        if clients[room] == room:
-            sock.send(bytes(prefix, "utf8")+msg)            
+    print(clients)
+    for user in clients.values():
+        if user['room'] == room:
+            user['socket'].send(bytes(prefix, "utf8")+msg)
+
+        
+clients = {}
+addresses = {}
+
+HOST = ''
+PORT = 4200
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
 
 if __name__ == "__main__":
     SERVER.listen(5)
