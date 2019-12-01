b840193718aad8b56a117810ce148472533ef5db
msharif9 <46731860+msharif9@users.noreply.github.com>
2019-12-01 16:07:32 -0500

    Graceful-shutdown implemented
    ### Terminate serverThread ###
    Create local connection to server to unblock server.accept().
    For each socketThread created by serverThread call terminate() to terminate thread
    
    ### Terminate socketThread ###
    Call shutdown(SHUT_RD) and close() on clientSocket to close the socket for each socketThread

--- server.py
+++ server.py
@@ -1,54 +1,61 @@
 from socket import AF_INET, socket, SOCK_STREAM
-import socketserver
+from socket import SHUT_RD, SHUT_WR, SHUT_RDWR
 from threading import Thread
 import threading
 
 class socketThread(Thread):
     def __init__(self, client):
-        self._running = False
         self.stop = threading.Event()
+        self._running_flag = False
         self.clientSocket = client
         Thread.__init__(self, target=self.threadMain)
-        print('init')
     
-    def threadMain(self): 
-        # Handles each client request
-        print('test')
-        print(self.clientSocket)
-        client = self.clientSocket
-        client.send(bytes("Please enter your username", "utf8"))
-        name = client.recv(buffer).decode("utf8")
-        welcome = 'Type {disconnect} to exit.'
+    def threadMain(self):
+
+        first = True
         try:
-            client.send(bytes(welcome, "utf8"))
+            while not self.stop.wait(0.1):
+                self._running_flag = True
+                if first:
+                    client = self.clientSocket
+                    client.send(bytes("Please enter your username", "utf8"))
+                    name = client.recv(buffer).decode("utf8")
+                    welcome = 'Type {disconnect} to exit.'
+                    first = False
+                    try:
+                        client.send(bytes(welcome, "utf8"))
+                    except:
+                        print('\nCould not send welcome message')
+                
+                    handle = {'socket' : client, 'room' : ''}
+                    clients[name] = handle
+                    roomPrompt = '\nPlease enter the chatroom you wish to join'
+                    try:
+                        client.send(bytes(roomPrompt, "utf8"))
+                    except:
+                        print("\nFailed to send room prompt message to client")
+                
+            
+                message = client.recv(buffer)
+                if clients[name]['room'] == '':
+                    room = message.decode("utf8")
+                    clients[name]['room'] = room
+                    message = "%s has joined %s!" % (name,room)
+                    self.broadcast(bytes(message, "utf8"),room)
+                elif message != bytes("{disconnect}", "utf8"):
+                    self.broadcast(message, room, name+": ")
+                else:
+                    try:
+                        client.send(bytes("{disconnect}", "utf8"))
+                    except:
+                        pass
+                    finally:
+                        self.broadcast(bytes("%s has left the chat." % name, "utf8"), room)
+                        break
         except:
-            print('\nCould not send welcome message')
-    
-        handle = {'socket' : client, 'room' : ''}
-        clients[name] = handle
-        roomPrompt = '\nPlease enter the chatroom you wish to join'
-        try:
-            client.send(bytes(roomPrompt, "utf8"))
-        except:
-            print("\nFailed to send room prompt message to client")
-    
-        while True:
-            message = client.recv(buffer)
-            if clients[name]['room'] == '':
-                room = message.decode("utf8")
-                clients[name]['room'] = room
-                message = "%s has joined %s!" % (name,room)
-                self.broadcast(bytes(message, "utf8"),room)
-            elif message != bytes("{disconnect}", "utf8"):
-                self.broadcast(message, room, name+": ")
-            else:
-                try:
-                    client.send(bytes("{disconnect}", "utf8"))
-                except:
-                    pass
-                finally:
-                    self.broadcast(bytes("%s has left the chat." % name, "utf8"), room)
-                break
+            pass
+        finally:
+            self._running_flag = False
     def broadcast(self, msg, room, prefix=""):  # prefix is for name identification.
         """Broadcasts a message to all the clients."""
         for user in clients.values():
@@ -59,37 +66,52 @@ class socketThread(Thread):
                     print("Client " +str(user['socket'])+ " has disconnected")
 
     def terminate(self):
+        self.clientSocket.shutdown(SHUT_RD)
+        self.clientSocket.close()
         self.stop.set()
             
 
 class serverThread(Thread):
     def __init__(self):
-        self._running = False
-        self._sockets = list()
         self.stop = threading.Event()
+        self._running_flag = False
         Thread.__init__(self, target=self.threadMain)
 
     def threadMain(self):
         # Handles each incoming connection request
-        while not self.stop.wait(1):
-            print('running')
-            try: 
-                self._running = True
-                client, client_address = server.accept()
-                print("%s:%s has connected." % client_address)
-                addresses[client] = client_address
-                socketThread(client)
-            except:
-                print("\nFailed to connect to client")
-            finally:
-                self._running = False
+        counter = 0
+        try:
+            while not self.stop.wait(0.1):
+                self._running_flag = True
+                try: 
+                    client, client_address = server.accept()
+                    print("%s:%s has connected." % client_address)
+                    addresses[client] = client_address
+                    temp = socketThread(client)
+                    temp.start()
+                    _sockets[str(counter)] = temp
+                    print(temp)
+                    print(_sockets)
+                except:
+                    print("\nFailed to connect to client")
+        finally:
+                self._running_flag = False
+            
                 
     def terminate(self):
+        try:
+            for thread in _sockets.values():
+                thread.terminate()
+                thread.join()
+        except:
+            print('No socketThreads to terminate')
+        socket(AF_INET, SOCK_STREAM).connect(('localhost',4200))
         self.stop.set()
 
         
 clients = {}
 addresses = {}
+_sockets = {}
 
 host = ''
 port = 4200
