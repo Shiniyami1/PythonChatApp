07641f50f5b6be42e8a4d577c9c89ab5eb54c9ae
msharif9 <46731860+msharif9@users.noreply.github.com>
2019-11-30 19:13:21 -0500

    threadMain for socketThread
    threadMain does not execute

--- server.py
+++ server.py
@@ -3,22 +3,82 @@ import socketserver
 from threading import Thread
 import threading
 
+class socketThread(Thread):
+    def __init__(self, client):
+        self._running = False
+        self.stop = threading.Event()
+        self.clientSocket = client
+        Thread.__init__(self, target=self.threadMain)
+        print('init')
+    
+    def threadMain(self): 
+        # Handles each client request
+        print('test')
+        print(self.clientSocket)
+        client = self.clientSocket
+        client.send(bytes("Please enter your username", "utf8"))
+        name = client.recv(buffer).decode("utf8")
+        welcome = 'Type {disconnect} to exit.'
+        try:
+            client.send(bytes(welcome, "utf8"))
+        except:
+            print('\nCould not send welcome message')
+    
+        handle = {'socket' : client, 'room' : ''}
+        clients[name] = handle
+        roomPrompt = '\nPlease enter the chatroom you wish to join'
+        try:
+            client.send(bytes(roomPrompt, "utf8"))
+        except:
+            print("\nFailed to send room prompt message to client")
+    
+        while True:
+            message = client.recv(buffer)
+            if clients[name]['room'] == '':
+                room = message.decode("utf8")
+                clients[name]['room'] = room
+                message = "%s has joined %s!" % (name,room)
+                self.broadcast(bytes(message, "utf8"),room)
+            elif message != bytes("{disconnect}", "utf8"):
+                self.broadcast(message, room, name+": ")
+            else:
+                try:
+                    client.send(bytes("{disconnect}", "utf8"))
+                except:
+                    pass
+                finally:
+                    self.broadcast(bytes("%s has left the chat." % name, "utf8"), room)
+                break
+    def broadcast(self, msg, room, prefix=""):  # prefix is for name identification.
+        """Broadcasts a message to all the clients."""
+        for user in clients.values():
+            if user['room'] == room:
+                try:
+                    user['socket'].send(bytes(prefix, "utf8")+msg)
+                except:
+                    print("Client " +str(user['socket'])+ " has disconnected")
+
+    def terminate(self):
+        self.stop.set()
+            
+
 class serverThread(Thread):
     def __init__(self):
         self._running = False
+        self._sockets = list()
         self.stop = threading.Event()
         Thread.__init__(self, target=self.threadMain)
 
     def threadMain(self):
         # Handles each incoming connection request
         while not self.stop.wait(1):
+            print('running')
             try: 
                 self._running = True
                 client, client_address = server.accept()
                 print("%s:%s has connected." % client_address)
-                client.send(bytes("Please enter your username", "utf8"))
                 addresses[client] = client_address
-                Thread(target=socketThread, args=(client,)).start()
+                socketThread(client)
             except:
                 print("\nFailed to connect to client")
             finally:
@@ -26,56 +86,6 @@ class serverThread(Thread):
                 
     def terminate(self):
         self.stop.set()
-
-
-def socketThread(client): 
-    # Handles each client request
-
-    name = client.recv(buffer).decode("utf8")
-    welcome = 'Type {disconnect} to exit.'
-    try:
-        client.send(bytes(welcome, "utf8"))
-    except:
-        print('\nCould not send welcome message')
-    
-    handle = {'socket' : client, 'room' : ''}
-    clients[name] = handle
-    roomPrompt = '\nPlease enter the chatroom you wish to join'
-    try:
-        client.send(bytes(roomPrompt, "utf8"))
-    except:
-        print("\nFailed to send room prompt message to client")
-    
-
-    while True:
-        message = client.recv(buffer)
-        if clients[name]['room'] == '':
-            room = message.decode("utf8")
-            clients[name]['room'] = room
-            message = "%s has joined %s!" % (name,room)
-            broadcast(bytes(message, "utf8"),room)
-        elif message != bytes("{disconnect}", "utf8"):
-            broadcast(message, room, name+": ")
-        else:
-            try:
-                client.send(bytes("{disconnect}", "utf8"))
-            except:
-                pass
-            finally:
-                broadcast(bytes("%s has left the chat." % name, "utf8"), room)
-            break
-            
-
-
-def broadcast(msg, room, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-
-    for user in clients.values():
-        if user['room'] == room:
-            try:
-                user['socket'].send(bytes(prefix, "utf8")+msg)
-            except:
-                print("Client " +str(user['socket'])+ " has disconnected")
 
         
 clients = {}
@@ -94,12 +104,8 @@ if __name__ == "__main__":
     print("Waiting for connections...\n")
     server_thread = serverThread()
     server_thread.start()
-    
-    
-    while True:
-        terminate = input("Enter 'shutdown' to terminate server\n")
-        if terminate == 'shutdown':
-            server_thread.terminate()
-            server_thread.join()
-            break
+    terminate = input("Enter 'shutdown' to terminate server\n")
+    if terminate == 'shutdown':
+        server_thread.terminate()
+        server_thread.join()
     server.close()
