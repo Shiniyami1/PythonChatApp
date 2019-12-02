a32e7eac6adf39a32cdbd1bcd7b04e4afc972edd
TJ Swaggarwal <taggarw@uwo.ca>
2019-12-01 21:25:44 -0500

    sound effects completed

--- client.py
+++ client.py
@@ -5,72 +5,74 @@ import socketserver
 from threading import Thread
 import tkinter, time, pygame
 
-
+pygame.mixer.init()
 def receive():
     """Handles receiving of messages."""
     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg = client_socket.recv(buffer_size).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-            pygame.mixer.init()
-            recvNotification = pygame.mixer.Sound('when.wav')
-            recvNotification.play()
+            if 'has joined' in msg:
+                sendNotification = pygame.mixer.Sound('eventually.wav')
+                sendNotification.play()
+            elif 'has left the chat.' in msg:
+                sendNotification = pygame.mixer.Sound('deduction.wav')
+                sendNotification.play()
+            else:
+                recvNotification = pygame.mixer.Sound('when.wav')
+                recvNotification.play()
         except OSError:  # Possibly client has left the chat.
             break
 
 
 def send(event=None):  # event is passed by binders.
     """Handles sending of messages."""
-    msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
+    msg = userInput.get()
+    userInput.set("")  # Clears input field.
     client_socket.send(bytes(msg, "utf8"))
 
     if msg == "{disconnect}":
-        pygame.mixer.init()
-        sendNotification = pygame.mixer.Sound('eventually.wav')
-        sendNotification.play()
         client_socket.close()
         top.quit()
-            
-
 
 def on_closing(event=None):
     """This function is to be called when the window is closed."""
-    my_msg.set("{disconnect}")
+    userInput.set("{disconnect}")
     send()
 
 top = tkinter.Tk()
 top.title("SE3313 Project 2019 Chat")
 
-messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
+msgBox = tkinter.Frame(top)
+userInput = tkinter.StringVar()  # For the messages to be sent.
 # my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=30, width=100, yscrollcommand=scrollbar.set)
+scrollbar = tkinter.Scrollbar(msgBox)  # To navigate through past messages.
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+# Following will contain the messages.
+msg_list = tkinter.Listbox(msgBox, height=30, width=100, yscrollcommand=scrollbar.set)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
-messages_frame.pack()
+#msg_list.pack()
+msgBox.pack()
 
-entry_field = tkinter.Entry(top, textvariable=my_msg)
-entry_field.bind("<Return>", send)
-entry_field.pack(ipadx=175)
+inputBar = tkinter.Entry(top, textvariable=userInput)
+inputBar.bind("<Return>", send)
+inputBar.pack(ipadx=175)
 send_button = tkinter.Button(top, text="Send", command=send)
 send_button.pack()
+
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
 #----Now comes the sockets part----
-HOST = 'localhost'
-PORT = 4200
+host_addr = 'localhost'
+port_num = 4200
 
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+buffer_size = 1024
+conn_addr = (host_addr, port_num)
 
 client_socket = socket(AF_INET, SOCK_STREAM)
 try:
-    client_socket.connect(ADDR)
+    client_socket.connect(conn_addr)
 except (ConnectionRefusedError, ConnectionRefusedError, ConnectionError) as error:
     msg_list.insert(tkinter.END, 'Client failed to connect to server!')
 
